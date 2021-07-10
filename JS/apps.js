27fa0b2e6ef352c334f0b360eeb4b5a4b54fe7b2
Your Name <you@example.com>
2021-07-09 19:57:34 -0600

    array strings and loop

---
+++ JS/apps.js
@@ -0,0 +1,21 @@
+var tweets = [
+    "Not sure if I actually like movies or just like looking at something whilie I eat popcorn",
+    "Just choked on an apple seed. This is what I get for trying to eat healthy.",
+    "Debugging is like an onion. There are multiple layers to it, and the more you peel them back, the more likely you're going to start crying at inappropriate times",
+    "The closest I have been to a diet this year is erasing food searches from my browser history",
+    "My bucket list is basically a list of places I want to eat",
+    "Winter is just a 3 month long eating contest",
+    "The best debugger ever made is a good night's sleep",
+    "I can't turn water into wine, but I can turn ice cream into breakfast",
+    "Why scoop ice cream into a bowl when you can just scoop it straight into you mouth",
+    "Dogs are cooler than cats"
+]
+
+var arrayLength = tweets.length;
+
+for (counter = 0; counter < tweets.length; counter++) {
+    console.log(tweets[counter]);
+    console.log();
+}
+
+
